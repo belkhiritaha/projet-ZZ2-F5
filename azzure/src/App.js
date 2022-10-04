73e6ce3c8c5dfb1129b389e7254200c0e2185832
Taha Belkhiri <belkhiri.taha53@gmail.com>
2022-10-04 15:14:13 +0200

    added main content container, to do: add sidebar and main content div

--- azzure/src/App.js
+++ azzure/src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import BasicExample from './navbar';
+import Container from 'react-bootstrap/Container';
 
 function App() {
     // return navbar
     return (
-        <BasicExample />
+        <>
+            <BasicExample />
+            <Container>
+                <h1>Test</h1>
+            </Container>
+        </>
     );
 }
 
