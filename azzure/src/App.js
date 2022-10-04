cc2f2a0fede766aed4ed9691629c191e1bf895c5
Taha Belkhiri <belkhiri.taha53@gmail.com>
2022-10-04 14:48:59 +0200

    installed react and setup first working example

---
+++ azzure/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import BasicExample from './navbar';
+
+function App() {
+    // return navbar
+    return (
+        <BasicExample />
+    );
+}
+
+export default App;
