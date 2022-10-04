e05a2ea40542e1ec0e6b49d58b1394ee9b113cf1
Taha Belkhiri <belkhiri.taha53@gmail.com>
2022-10-04 22:37:44 +0200

    new feature: customizable card items

--- azzure/src/card.js
+++ azzure/src/card.js
@@ -2,26 +2,38 @@ import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 import './card.css'
 
+
+function CustomListGroup(props) {
+    const nbItems = props.nbFields;
+    const items = [];
+    for (let i = 1; i <= nbItems; i++) {
+        console.log('item' + i);
+        items.push(<ListGroup.Item>{props['item' + i]}</ListGroup.Item>);
+    }
+    console.log(items);
+    return (
+        <ListGroup className="list-group-flush">
+            {items.map((item) => item)}
+        </ListGroup>
+    );
+}
+
+
 function CardItem(props) {
-  return (
-    <Card style={{ width: '18rem', margin: 'auto' }}>
-      {/* <Card.Img variant="top" src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/77/Google_Images_2015_logo.svg/1280px-Google_Images_2015_logo.svg.png" /> */}
-      <i className={props.img} style={{ fontSize: '5rem', margin: 'auto', marginTop: '1rem' }}></i>
-      <Card.Body>
-        <Card.Title>{props.title}</Card.Title>
-        <Card.Text>{props.text}</Card.Text>
-      </Card.Body>
-      <ListGroup className="list-group-flush">
-        <ListGroup.Item>Cras justo odio</ListGroup.Item>
-        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-      </ListGroup>
-      <Card.Body>
-        <Card.Link href="#">Card Link</Card.Link>
-        <Card.Link href="#">Another Link</Card.Link>
-      </Card.Body>
-    </Card>
-  );
+    const nbItems = props.nbFields;
+
+    return (
+        <Card style={{ width: '18rem', margin: 'auto' }}>
+            {/* <Card.Img variant="top" src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/77/Google_Images_2015_logo.svg/1280px-Google_Images_2015_logo.svg.png" /> */}
+            <i className={props.img} style={{ fontSize: '5rem', margin: 'auto', marginTop: '1rem' }}></i>
+            <Card.Body>
+                <Card.Title>{props.title}</Card.Title>
+                <Card.Text>{props.text}</Card.Text>
+            </Card.Body>
+            <CustomListGroup {...props} />
+            {/* <CustomListGroup nbFields={nbItems} item0={props.item1} item1={props.item1} /> */}
+        </Card>
+    );
 }
 
 export default CardItem;
--- azzure/src/mainbar.js
+++ azzure/src/mainbar.js
@@ -21,10 +21,10 @@ function Mainbar() {
                     <h2>Clermont INP's IoT VM Manager</h2>
                 </Container>
                 <Row>
-                    <CardItem title="Create a VM" text="Create a customized VM to store and process your IoT data" img="fas fa-cloud-upload-alt"/>
-                    <CardItem title="View my VMs" text="View your existing VMs" img="fas fa-desktop"/>
-                    <CardItem title="Run a Python script" text="Run a Python script on your VM" img="fas fa-terminal"/>
-                    <CardItem title="View my Sensors" text="View your existing sensors" img="fas fa-microchip"/>
+                    <CardItem title="Create a VM" text="Create a customized VM to store and process your IoT data" img="fas fa-cloud-upload-alt" item1="Import a configuration" item2="Create a new configuration" nbFields="2"/>
+                    <CardItem title="View my VMs" text="View your existing VMs" img="fas fa-desktop" item1="View 'vm_name1'" item2="View 'vm_name2'" item3="View 'vm_name3'" nbFields="3"/>
+                    <CardItem title="Run a Python script" text="Run a Python script on your VM" img="fas fa-terminal" item1="Select a script" item2="Upload a new script" nbFields="2" />
+                    <CardItem title="View my Sensors" text="View your existing sensors" img="fas fa-microchip" item1="View 'sensor_name1'" item2="View 'sensor_name2'" item3="View 'sensor_name3'" item4="View 'sensor_name4'" nbFields="4" />
                     <CardItem title="Dashboard" text="Vizualise your IoT data" img="fas fa-chart-line"/>
                 </Row>
 
