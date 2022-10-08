0b14ae6d8b3806babd0bc23b46488d9d8cd022a5
Taha Belkhiri <belkhiri.taha53@gmail.com>
2022-10-08 11:58:50 +0200

    working on cards onClick function

--- azzure/src/mainbar.js
+++ azzure/src/mainbar.js
@@ -1,7 +1,9 @@
+import React, { useState } from 'react';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Container from 'react-bootstrap/Container';
 import CardItem from './card';
+import { Collapse } from 'react-bootstrap';
 import './mainbar.css';
 
 function MainbarItem(props) {
@@ -13,6 +15,28 @@ function MainbarItem(props) {
 }
 
 function Mainbar() {
+    // shown menu state
+    const [menu, setMenu] = useState('home');
+
+    const rowContent = [];
+    // menu items
+    switch (menu) {
+        case 'home':
+            rowContent.push(<CardItem onHover={() => {console.log("create")}} style={{ cursor: "pointer"}} key={rowContent.length + 1} card_id='1' title="Create a VM" text="Create a customized VM to store and process your IoT data" img="fas fa-cloud-upload-alt" item1="Import a configuration" item2="Create a new configuration" nbFields="2"/>);
+            rowContent.push(<CardItem key={rowContent.length + 1} card_id='2' title="Manage your VMs" text="Manage your VMs" img="fas fa-desktop" item1="Start a VM" item2="Stop a VM" item3="Delete a VM" nbFields="3"/>);
+            rowContent.push(<CardItem key={rowContent.length + 1} card_id='1' title="Run a Python script" text="Run a Python script on your VM" img="fas fa-terminal" item1="Select a script" item2="Upload a new script" nbFields="2" />);
+            rowContent.push(<CardItem key={rowContent.length + 1} card_id='1' title="View my Sensors" text="View your existing sensors" img="fas fa-microchip" item1="View 'sensor_name1'" item2="View 'sensor_name2'" item3="View 'sensor_name3'" item4="View 'sensor_name4'" nbFields="4" />);
+            rowContent.push(<CardItem key={rowContent.length + 1} card_id='1' title="Dashboard" text="Vizualise your IoT data" img="fas fa-chart-line"/>);
+            break;
+
+        case 'create':
+            rowContent.push(<CardItem key={rowContent.length + 1} card_id='1' title="Create a VM" text="Create a customized VM to store and process your IoT data" img="fas fa-cloud-upload-alt" item1="Import a configuration" item2="Create a new configuration" nbFields="2"/>);
+            break;
+    
+        default:
+            break;
+    }
+
     return (
         <>
             <Col xs={10}>
@@ -20,12 +44,11 @@ function Mainbar() {
                     <h1>aZZure</h1>
                     <h2>Clermont INP's IoT VM Manager</h2>
                 </Container>
+
                 <Row>
-                    <CardItem card_id='1' title="Create a VM" text="Create a customized VM to store and process your IoT data" img="fas fa-cloud-upload-alt" item1="Import a configuration" item2="Create a new configuration" nbFields="2"/>
-                    <CardItem card_id='1' title="View my VMs" text="View your existing VMs" img="fas fa-desktop" item1="View 'vm_name1'" item2="View 'vm_name2'" item3="View 'vm_name3'" nbFields="3"/>
-                    <CardItem card_id='1' title="Run a Python script" text="Run a Python script on your VM" img="fas fa-terminal" item1="Select a script" item2="Upload a new script" nbFields="2" />
-                    <CardItem card_id='1' title="View my Sensors" text="View your existing sensors" img="fas fa-microchip" item1="View 'sensor_name1'" item2="View 'sensor_name2'" item3="View 'sensor_name3'" item4="View 'sensor_name4'" nbFields="4" />
-                    <CardItem card_id='1' title="Dashboard" text="Vizualise your IoT data" img="fas fa-chart-line"/>
+                    <CardItem tag="a" onClick={() => {console.log("create")}} style={{ cursor: "pointer"}} key={rowContent.length + 1} card_id='1' title="Create a VM" text="Create a customized VM to store and process your IoT data" img="fas fa-cloud-upload-alt" item1="Import a configuration" item2="Create a new configuration" nbFields="2"/>
+
+                    {rowContent}
                 </Row>
 
             </Col>
