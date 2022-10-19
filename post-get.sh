c117efeccb669763625377f41fccc638a22a0e3c
abmouhalha <113850033+abmouhalha@users.noreply.github.com>
2022-10-19 09:36:12 +0200

    Update post-get.sh

--- post-get.sh
+++ post-get.sh
@@ -5,7 +5,7 @@ echo "Contenu de la base redis avant POST"
 curl localhost:80
 
 echo "POST..."
-curl --header "Content-Type: application/json" --request POST --data '{"name":"xavki blog"}' localhost:80
+curl --header "Content-Type: application/json" --request POST --data '{"name":"Abdel"}' localhost:80
 
 echo "Contenu de la base redis après POST"
 curl localhost:80
