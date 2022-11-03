78a956d885642bfbeec340396185941ce243e8d4
loelkhalid <lotfi.elkhalidy@gmail.com>
2022-11-03 13:15:36 +0100

    Added routes to add, delete and update users

--- APIdatabase/app.js
+++ APIdatabase/app.js
@@ -1,6 +1,6 @@
 const  express = require("express")
 const mongoose = require("mongoose")
-const model = require("./models")
+const User = require("./models")
 const app = express()
 
 mongoose.connect("mongodb+srv://aZZure:admin@cluster0.tm9ghpd.mongodb.net/?retryWrites=true&w=majority", {
@@ -16,29 +16,45 @@ mongoose.connect("mongodb+srv://aZZure:admin@cluster0.tm9ghpd.mongodb.net/?retry
 
 app.use(express.json())
 
-/*
-app.post("/", async (req, res) => {    
-    const user = new model(req.body)
+// POST user
+app.post('/api/user', (req, res) => {
+    //delete req.body._id    
+    const user = new User({
+        ...req.body
+    })
 
-    try {
-        await user.save()
-        res.send(user)
-    }
-    catch(error) {
-        response.status(500).send(error)
-    }
+    user.save()
+        .then(() => res.status(201).json({ message: 'A new user has arrived !' }))
+        .catch(error => res.status(400).json({ error }))
 })
 
-app.get("/users", async (req, res) => {
-    const users = await model.find({})
-
-    try {
-        res.send(users)
-    }
-    catch(error) {
-        response.status(500).send(error)
-    }
+// GET user by ID
+app.get('/api/user/:id', (req, res) => {
+    User.findOne({_id: req.params.id})
+        .then(user => res.status(200).json(thing))
+        .catch(error => res.status(404).json({ error }))
 })
-*/
+
+// GET all users
+app.get('/api/user', (req, res) => {
+    console.log(req.body)
+    User.find()
+        .then(users => res.status(200).json(users))
+        .catch(error => res.status(400).json({ error }))
+})
+
+// UPDATE the user's data
+app.put('/api/user/:id', (req, res) => {
+    User.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'The data of the user has been modified !' }))
+        .catch(error => res.status(400).json({ error }))
+})
+
+// DELETE user by ID
+app.delete('/api/user/:id', (req, res) => {
+    User.deleteOne({_id: req.params.id})
+        .then(user => res.status(200).json({ message: 'The user has been deleted !' }))
+        .catch(error => res.status(400).json({ error }))
+})
 
 module.exports = app
--- APIdatabase/server.js
+++ APIdatabase/server.js
@@ -48,7 +48,7 @@ server.on('error', errorHandler)
 server.on('listening', () => {
     const address = server.address()
     const bind = typeof address === 'string' ? 'pipe' + address : 'port: ' + port
-    console.log('Listening on port ' + bind)
+    console.log('Listening on ' + bind)
 })
 
 server.listen(port)
