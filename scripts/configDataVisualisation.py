738c17e3929c6312074c7e9bd341e737a626059d
Thomas <thomas.galpin@etu.uca.fr>
2022-11-09 09:28:14 +0100

    add gitignore + add env variable for grafana

--- scripts/configDataVisualisation.py
+++ scripts/configDataVisualisation.py
@@ -2,17 +2,24 @@
 def configDataVisualisation(contenu, technology, database, pathToTechnology):
 
     contenu[0]['services'][technology] = {
-                              'image': technology + '/' + technology, 
-                              'depends_on': [database],                     # le système de database est nécessaire
-                              'container_name': technology, 
-                              'restart': 'always',
-                              'ports': ['3000:3000'],                       # port pour accéder à l'interface web de Grafana
-                              'links': [database +':'+ database], 
-                              'volumes':[pathToTechnology]
-                             }
+        'image': technology + '/' + technology,
+        # le système de database est nécessaire
+        'depends_on': [database],
+        'container_name': technology,
+        'restart': 'always',
+        # port pour accéder à l'interface web de Grafana
+        'ports': ['3000:3000'],
+        'links': [database + ':' + database],
+        'volumes': [pathToTechnology]
+    }
+
+    envUser = "GF_SECURITY_ADMIN_USER : $USER"
+    envPasswd = "GF_SECURITY_ADMIN_PASSWORD : $PASSWORD"
+
+    if technology == "grafana":
+        contenu[0]['services'][technology]['environnement'] = [envUser, envPasswd]
 
     print("C'EST")
 
 
-
-#formatiser le path de la techno qu'on veut pour visualiser la data
+# formatiser le path de la techno qu'on veut pour visualiser la data
