913ba2766beda7a4542de0f9f818cc05d2e15ec1
loelkhalid <lotfi.elkhalidy@gmail.com>
2022-11-21 16:14:08 +0100

    Added the onSubmit function

--- azzure/src/login.js
+++ azzure/src/login.js
@@ -4,29 +4,48 @@ import NavbarBasicExample from './navbar';
 import './login.css';
 
 function Login() {
-  return (
-    <>
-        <NavbarBasicExample />
-        <div className='container'>
-            <h2>Login</h2>
-            <Form>
-                <Form.Group className="mb-3" controlId="formBasicUsername">
-                    <Form.Label>Enter username :</Form.Label>
-                    <Form.Control type="text" placeholder="Username..." />
-                </Form.Group>
+    let username = '';
+    let email = '';
+    let password = '';
+    
+    function handleUsernameChange(event) {
+        username = event.target.value;
+        console.log(username);
+    }
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Enter password :</Form.Label>
-                    <Form.Control type="password" placeholder="******" />
-                </Form.Group>
+    function handlePasswordChange(event) {
+        password = event.target.value;
+        console.log(password);
+    }
 
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
-            </Form>
-        </div>
-    </>
-  );
+    function onsubmit(event) {
+        event.preventDefault();
+        console.log('submit');
+    }
+
+    return (
+        <>
+            <NavbarBasicExample />
+            <div className='container'>
+                <h2>Login</h2>
+                <Form onSubmit={onsubmit}>
+                    <Form.Group className="mb-3" controlId="formBasicUsername">
+                        <Form.Label>Enter username :</Form.Label>
+                        <Form.Control type="text" placeholder="Username..." onChange={handleUsernameChange} />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Enter password :</Form.Label>
+                        <Form.Control type="password" placeholder="******" onChange={handlePasswordChange} />
+                    </Form.Group>
+
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Form>
+            </div>
+        </>
+    );
 }
 
 export default Login;
