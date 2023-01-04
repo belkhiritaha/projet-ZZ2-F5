c5de2799e9d0ba800d176ff05023b58c49cd7069
Topin2001 <wonodoou00@gmail.com>
2023-01-04 14:15:56 +0100

    create install script

---
+++ install.sh
@@ -0,0 +1,19 @@
+#/bin/bash
+
+#Install curl and docker file
+sudo apt update -y && sudo apt install curl docker.io docker-compose
+
+#Install minikube and clear after install
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+rm -rf minikube-linux-amd64
+
+#Install kubectl and clear after install
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+
+#Install Kompose
+curl -L https://github.com/kubernetes/kompose/releases/download/v1.26.0/kompose-linux-amd64 -o kompose
+chmod +x kompose
+sudo mv ./kompose /usr/local/bin/kompose
