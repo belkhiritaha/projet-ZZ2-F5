ebd0e5f15897ba735e5cf7c2a914b421d4bf1af5
Topin2001 <wonodoou00@gmail.com>
2023-01-04 16:52:07 +0100

    split docker script

--- start_docker.py
+++ start_docker.py
@@ -1,26 +1,16 @@
 #!/usr/bin/env python
 import subprocess
-import glob
-
 
 def main():
     print("Quels docker-compose ?")
     path = input()
-    path_docker = path + 'docker-compose.yml'
     path_kube = path + 'kube_files/'
-    print (path_docker, path)
-
-    #Create kubernetes .yaml files
-    result = subprocess.run(["kompose", "convert", "-f", path_docker])
-    create_dir = subprocess.run(["mkdir", path_kube])
-    move = subprocess.run(['mv'] + glob.glob('*.yaml') + [path_kube])
+    print (path_kube, path)
 
     #Start the docker into K8s
     kubectl = subprocess.run(["kubectl", "apply", "-f", path_kube])
 
     return 0
-
-
 
 
 if __name__ == '__main__':
