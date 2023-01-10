566d5d6157a5430c2875e8144e96690a18f01211
Topin2001 <wonodoou00@gmail.com>
2023-01-10 12:03:00 +0100

    improve path verification

--- error_management/create_app.py
+++ error_management/create_app.py
@@ -2,27 +2,10 @@
 import subprocess
 from pathlib import Path
 
-class DirectoryExistingError(Exception):
-    "Raised when their is an existing error with the wanted directory"
-    pass
-
-class NotADirectoryError(Exception):
-    "Raised when the wanted directory is not a directory"
-    pass
-
 def create_app(user, app):
-    path_user = Path('users/' + user + '/')
-    if (path_user.exists()):
-        if path_user.is_dir():
-            path_app = Path('users/' + user + '/' + app)
-            if(path_app.exists()):
-                if(path_app.is_dir()):
-                    raise DirectoryExistingError("This application already exists")
-                else: raise NotADirectoryError("This application does not correspond to a directory")
-            else : 
-                #Create the app dir into the user dir
-                create_app = subprocess.run(["mkdir", path_app])
-        else : raise NotADirectoryError("This user does not correspond to a directory")
-    else : raise DirectoryExistingError("This user doesn't have a directory")
+    path_app = Path('users/' + user + '/' + app)
+    #Create the app dir into the user dir
+    create_app = subprocess.run(["mkdir", path_app])
+
                 
     return 0
--- error_management/main.py
+++ error_management/main.py
@@ -1,18 +1,45 @@
 import sys
 from pathlib import Path
 import subprocess
-from create_app import create_app, DirectoryExistingError, NotADirectoryError
+from create_app import create_app
 from create_kube_files import create_kube_files
 from start_docker import start_docker
 
+#Création of custom error
+class DirectoryExistingError(Exception):
+    "Raised when their is an existing error with the wanted directory"
+    pass
+
+class NotADirectoryError(Exception):
+    "Raised when the wanted directory is not a directory"
+    pass
+
+def try_path_new_app(user, app):
+    path_user = Path('users/' + user + '/')
+    if (path_user.exists()):
+        if path_user.is_dir():
+            path_app = Path('users/' + user + '/' + app)
+            if(path_app.exists()):
+                if(path_app.is_dir()):
+                    raise DirectoryExistingError("This application already exists")
+                else: raise NotADirectoryError("This application does not correspond to a directory")
+        else : raise NotADirectoryError("This user does not correspond to a directory")
+    else : raise DirectoryExistingError("This user doesn't have a directory")
+
 
 def main(user, app):
+
     #Création du dossier application dans le dossier du user
     try :
-        create_app(user, app)
+        try_path_new_app(user, app)
     except (DirectoryExistingError, NotADirectoryError) as err:
         print(err.args[0])
         return 1
+        
+    create_app(user, app)
+    path_app = Path('users/' + user + '/' + app)
+
+    copy = subprocess.run(["cp", "users/docker-compose.yml", path_app])
 
     #Création du docker-compose
     """try:
